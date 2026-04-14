eda62fd298bf713de73cfae17625af563214ffd7
Uvesh Menpurwala <uveshmenpur.03@gmail.com>
2026-04-14 17:41:51 +0530

    challenge two started.

---
+++ lib/challenges/challenge_two.dart
@@ -0,0 +1,10 @@
+import 'package:flutter/material.dart';
+
+class ChallengeTwo extends StatelessWidget {
+  const ChallengeTwo({super.key});
+
+  @override
+  Widget build(BuildContext context) {
+    return Container();
+  }
+}
--- lib/registry/challenge_model.dart
+++ lib/registry/challenge_model.dart
@@ -1,5 +1,6 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_ui_challenges/challenges/challenge_one.dart';
+import 'package:flutter_ui_challenges/challenges/challenge_two.dart';
 
 class Challenge {
   final String title;
@@ -11,4 +12,7 @@ class Challenge {
   const Challenge({required this.title, required this.category, required this.builder, required this.description, this.isMobile = true});
 }
 
-final List<Challenge> registry = [Challenge(title: 'Screen', category: 'Screen', builder: (_) => const ChallengeOne(), description: '')];
+final List<Challenge> registry = [
+  Challenge(title: 'Challenge One', category: '100 Days', builder: (_) => const ChallengeOne(), description: ''),
+  Challenge(title: 'Challenge Two', category: '100 Days', builder: (_) => const ChallengeTwo(), description: ''),
+];
