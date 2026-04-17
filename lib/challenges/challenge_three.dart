ee4dba76b41fc3ae71bbed19d1f42df5cfbc411e
Uvesh Menpurwala <uveshmenpur.03@gmail.com>
2026-04-17 16:18:00 +0530

    challenge 3 complete

--- lib/challenges/challenge_three.dart
+++ lib/challenges/challenge_three.dart
@@ -23,19 +23,26 @@ class ChallengeThree extends StatelessWidget {
             top: 30,
             bottom: 30,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.start,
               children: [
                 SvgPicture.asset('assets/svgs/svg_gear_vr.svg'),
                 Spacer(),
                 Column(
+                  crossAxisAlignment: CrossAxisAlignment.start,
                   children: [
                     Text(
                       'Gear VR',
                       style: TextStyle(fontSize: 30, fontWeight: FontWeight.bold, color: Colors.white),
                     ),
+                    SizedBox(height: 12),
                     Text('Design', style: TextStyle(fontSize: 24, color: Colors.white.withValues(alpha: 0.5))),
+                    SizedBox(height: 8),
                     Text('Display', style: TextStyle(fontSize: 24, color: Colors.white.withValues(alpha: 0.5))),
+                    SizedBox(height: 8),
                     Text('Experience', style: TextStyle(fontSize: 24, color: Colors.white.withValues(alpha: 0.5))),
+                    SizedBox(height: 8),
                     Text('Spec', style: TextStyle(fontSize: 24, color: Colors.white.withValues(alpha: 0.5))),
+                    SizedBox(height: 8),
                     Text('Gallery', style: TextStyle(fontSize: 24, color: Colors.white.withValues(alpha: 0.5))),
                   ],
                 ),
@@ -49,17 +56,18 @@ class ChallengeThree extends StatelessWidget {
             bottom: 0,
             right: 30,
             child: Column(
+              crossAxisAlignment: CrossAxisAlignment.end,
               children: [
                 Spacer(),
                 Text(
-                  'GEARVR',
-                  style: TextStyle(fontSize: 80, color: Colors.white, fontWeight: FontWeight.bold),
+                  'GEAR VR',
+                  style: TextStyle(fontSize: 100, color: Colors.white, fontWeight: FontWeight.w900),
                 ),
                 SizedBox(height: 10),
                 Text(
                   """It's easy to get lost in the world of\n virtual reality because the Gear VR\n is engineered to feel lighter.""",
                   textAlign: TextAlign.right,
-                  style: TextStyle(fontSize: 24),
+                  style: TextStyle(fontSize: 18, color: Colors.white),
                 ),
                 SizedBox(height: 25),
                 Container(
@@ -69,11 +77,12 @@ class ChallengeThree extends StatelessWidget {
                   ),
                   child: Center(
                     child: Padding(
-                      padding: EdgeInsetsGeometry.symmetric(horizontal: 16, vertical: 8),
+                      padding: EdgeInsetsGeometry.symmetric(horizontal: 20, vertical: 12),
                       child: Row(
                         children: [
-                          Text('Find out more', style: TextStyle(fontSize: 24, color: Colors.white)),
-                          Icon(Icons.forward),
+                          Text('Find out more', style: TextStyle(fontSize: 16, color: Colors.white)),
+                          SizedBox(width: 6),
+                          Icon(Icons.arrow_forward, color: Colors.white),
                         ],
                       ),
                     ),
