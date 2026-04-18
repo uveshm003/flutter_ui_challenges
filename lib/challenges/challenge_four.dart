44d80ee906c72201ff8a0a822b8e01cd6d54eb80
Uvesh Menpurwala <uveshmenpur.03@gmail.com>
2026-04-18 22:42:56 +0530

    Update challenge_four.dart

--- lib/challenges/challenge_four.dart
+++ lib/challenges/challenge_four.dart
@@ -5,6 +5,36 @@ class ChallengeFour extends StatelessWidget {
 
   @override
   Widget build(BuildContext context) {
-    return const Placeholder();
+    return Scaffold(
+      body: Container(
+        decoration: BoxDecoration(
+          gradient: LinearGradient(
+            colors: [Color(0XFF23A9FF), Color(0XFF014DFF)],
+            begin: AlignmentGeometry.topCenter,
+            end: AlignmentGeometry.bottomCenter,
+          ),
+        ),
+        child: Stack(
+          children: [
+            SizedBox(height: MediaQuery.sizeOf(context).height, width: MediaQuery.sizeOf(context).width),
+            Positioned(
+              right: -30,
+              left: -30,
+              top: -100,
+              bottom: MediaQuery.heightOf(context) * 0.6,
+              child: Container(
+                decoration: BoxDecoration(
+                  color: Color(0XFF073BFF),
+                  borderRadius: BorderRadius.only(
+                    bottomLeft: Radius.circular(MediaQuery.widthOf(context) * 0.5),
+                    bottomRight: Radius.circular(MediaQuery.widthOf(context) * 0.5),
+                  ),
+                ),
+              ),
+            ),
+          ],
+        ),
+      ),
+    );
   }
 }
